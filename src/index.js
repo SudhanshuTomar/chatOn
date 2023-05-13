1d7643b6d3b30ca5d6b3b4f1d658c83678055851
Sudhanshu <deepak@Deepaks-MacBook-Air.local>
2023-05-13 18:22:55 +0530

    created message template and rendered on site along with timestamp

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const express = require('express');
 //so we need to create a server outside of express 
 //as express does it behind the scenes and we dont have access to it
 const socketio = require('socket.io');
+const {generateMessage} = require('./utils/messages');
+
 
 const app = express();
 const Server = http.createServer(app);
@@ -17,6 +19,22 @@ app.use(express.static(publicDirectoryPath));
 
 io.on('connection',(socket)=>{
     console.log('New WebSocket connection');
+    
+    socket.emit('message',generateMessage('Welcome!'));
+    
+    socket.broadcast.emit('message',generateMessage('A new user has joined!'));
+    
+    socket.on('sendMessage',(message)=>{
+        io.emit('message',generateMessage(message));
+    })
+    socket.on('sendLocation',(coords)=>{
+        io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+    })
+
+    socket.on('disconnect',()=>{
+        io.emit('message',generateMessage('A user has left!'));
+    }
+    )
 })
 
 
