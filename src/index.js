e7f437f70446ce241dc29b24f196488672b0e10f
Sudhanshu <deepak@Deepaks-MacBook-Air.local>
2023-05-15 00:02:51 +0530

    ready for deployment

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const express = require('express');
 //as express does it behind the scenes and we dont have access to it
 const socketio = require('socket.io');
 const {generateMessage,generateLocationMessage} = require('./utils/messages');
-
+const {addUser,removeUser,getUser,getUsersInRoom} = require('./utils/users');
 
 const app = express();
-const Server = http.createServer(app);
-const io = socketio(Server);
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -19,32 +19,49 @@ app.use(express.static(publicDirectoryPath));
 
 io.on('connection',(socket)=>{
     console.log('New WebSocket connection');
+
+    socket.on('join',({username,room},callback)=>{
+        const {error,user} = addUser({id:socket.id,username,room})
+        if(error){
+            return callback(error)
+        }
+
+        socket.join(user.room)
+        socket.emit('message',generateMessage('Admin','Welcome!'));
     
-    
-    socket.on('join',({username,room})=>{
-        socket.join(room)
-        socket.emit('message',generateMessage('Welcome!'));
-    
-    socket.broadcast.to(room).emit('message',generateMessage('A new user has joined!'));
-    
+    socket.broadcast.to(user.room).emit('message',generateMessage("Admin",`${user.username} has joined!`));
+    io.to(user.room).emit('roomData',{
+        room:user.room,
+        users:getUsersInRoom(user.room)
+    })
+    callback()
+
     })
 
-    socket.on('sendMessage',(message)=>{
-        io.emit('message',generateMessage(message));
+    socket.on('sendMessage',(message, callback)=>{
+        const user = getUser(socket.id)
+        io.to(user.room).emit('message',generateMessage(user.username,message));
+        callback();
     })
-    socket.on('sendLocation',(coords)=>{
-        io.emit('message',generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
+    socket.on('sendLocation',(coords, callback)=>{
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage',generateLocationMessage(user.username,`https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
+        callback();
     })
 
     socket.on('disconnect',()=>{
-        io.emit('message',generateMessage('A user has left!'));
-    }
-    )
+        const user = removeUser(socket.id)
+        if(user){
+            io.to(user.room).emit('message',generateMessage(user.username,`${user.username} has left!`));
+            io.to(user.room).emit('roomData',{
+                room:user.room,
+                users:getUsersInRoom(user.room)
+            })
+        }
+
+    })
 })
 
-
-
-
-server.listen(port,(req,res)=>{
-    console.log('Server is up on port '+port);
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
 })
--- src/utils/users.js
+++ src/utils/users.js
@@ -1,42 +1,55 @@
-const users=[]
-//addUser, removeUser, getUser, getUsersInRoom
+const users = []
 
-const addUser = ({id, username, room})=>{
-    //clean the data
+const addUser = ({ id, username, room }) => {
+    // Clean the data
     username = username.trim().toLowerCase()
     room = room.trim().toLowerCase()
 
-    //validate the data
-    if(!username || !room){
+    // Validate the data
+    if (!username || !room) {
         return {
-            error:'Username and room are required!'
+            error: 'Username and room are required!'
         }
     }
 
-    //check for existing user
-    const existingUser = users.find((user)=>{
+    // Check for existing user
+    const existingUser = users.find((user) => {
         return user.room === room && user.username === username
     })
 
-    //validate username
-    if(existingUser){
+    // Validate username
+    if (existingUser) {
         return {
-            error:'Username is in use!'
+            error: 'Username is in use!'
         }
     }
 
-    //store user
-    const user = {id,username,room}
+    // Store user
+    const user = { id, username, room }
     users.push(user)
-    return {user}
+    return { user }
 }
 
-const removeUser = (id)=>{
-    const index = users.findIndex((user)=>user.id === id)
-    if(index !== -1){
-        return users.splice(index,1)[0]
-        //.splice(index,1) returns an array of the removed items
-        //1 represent the number of items to be removed
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
     }
 }
 
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
